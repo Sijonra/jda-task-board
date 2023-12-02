2b688fdee96bf6ac788514bedeb767f2997c1f85
Ilya Davydenko <ilya.davydenko@joy-dev.com>
2023-12-02 15:49:52 +0300

    feat[JDA-0302]: rendering cards from store in task-columns

--- src/pages/TaskBoardPage/TaskBoardPage.tsx
+++ src/pages/TaskBoardPage/TaskBoardPage.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from "react";
+import { FC, useState, useMemo } from "react";
 
 import styles from "./TaskBoardPage.module.scss";
 import classNames from "classnames/bind";
@@ -8,9 +8,54 @@ import BoardInput from "../../components/BoardInput/BoardInput";
 import BoardColumn from "../../components/BoardColumn/BoardColumn";
 import { TCardList } from "../../@types/types";
 
+const cardsList: TCardList = [
+	{
+		id: 0,
+		columnId: 0,
+		content: 'hello'
+	},
+	{
+		id: 1,
+		columnId: 0,
+		content: 'world'
+	},
+	{
+		id: 2,
+		columnId: 0,
+		content: 'cringe'
+	},
+	{
+		id: 3,
+		columnId: 1,
+		content: 'inProgress'
+	},
+	{
+		id: 4,
+		columnId: 2,
+		content: 'Done'
+	},
+	{
+		id: 5,
+		columnId: 2,
+		content: 'Done'
+	},
+]
+
 const TaskBoardPage: FC = () => {
 
-	const [cards, setCards] = useState<TCardList>();
+	const [cards, setCards] = useState<TCardList>(cardsList);
+
+	const toDoCards = useMemo(()=>{
+		return cards.filter(card => card.columnId === 0)
+	}, [cards])
+
+	const inProgressCards = useMemo(()=>{
+		return cards.filter(card => card.columnId === 1)
+	}, [cards])
+
+	const doneCards = useMemo(()=>{
+		return cards.filter(card => card.columnId === 2)
+	}, [cards])
 
 	return (
 		<div className={cx("task-board")}>
@@ -18,9 +63,9 @@ const TaskBoardPage: FC = () => {
 				<BoardInput />
 			</div>
 			<div className={cx("task-board__columns")}>
-				<BoardColumn />
-				<BoardColumn />
-				<BoardColumn />
+				<BoardColumn cards={toDoCards} />
+				<BoardColumn cards={inProgressCards} />
+				<BoardColumn cards={doneCards} />
 			</div>
 		</div>
 	);
