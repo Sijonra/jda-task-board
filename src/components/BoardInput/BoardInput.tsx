d2157201922ba0836b7567cf2a8f731a4e611e01
Ilya Davydenko <ilya.davydenko@joy-dev.com>
2023-12-02 17:16:33 +0300

    feat[JDA-0303]: add onChange handle for input to control input value

--- src/components/BoardInput/BoardInput.tsx
+++ src/components/BoardInput/BoardInput.tsx
@@ -4,13 +4,24 @@ import styles from "./BoardInput.module.scss";
 import classNames from "classnames/bind";
 const cx = classNames.bind(styles);
 
+import {useState} from 'react';
+
 import Input from "../Input/Input";
 import Button from "../Button/Button";
 
 const BoardInput: FC = () => {
+
+	const [inputValue, setInputValue] = useState('');
+
+	const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) =>{
+		setInputValue(event.target.value);
+	}
+
 	return (
 		<div className={cx("board-input")}>
 			<Input
+				onChange={handleInputChange}
+				value={inputValue}
 				className={cx("board-input__input")}
 				placeholder="add new task"
 			/>
